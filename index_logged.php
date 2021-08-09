5de28d347d7bc6fecd480f354f77e23fcc3200d6
KonkowIT <37418145+KonkowIT@users.noreply.github.com>
2021-08-09 13:47:06 +0200

    Update index_logged.php

--- index_logged.php
+++ index_logged.php
@@ -72,7 +72,7 @@ if ($date_arr->num_rows > 0) {
                     <?php
                     $records = mysqli_query($conn, "SELECT siec From desktop_ss GROUP BY siec;");
                     while ($data = mysqli_fetch_array($records)) {
-                        echo "<option value='" . $data['siec'] . "'>" . $data['siec'] . "</option>";
+                        echo "<option value='" . $data['siec'] . "' " . ($_GET['siec'] == $data['siec'] ? 'selected' : '') . ">" . $data['siec'] . "</option>";
                     };
                     ?>
                 </select>
@@ -83,9 +83,11 @@ if ($date_arr->num_rows > 0) {
             <div class="select">
                 <select class="select-text" required name='status'>
                     <option selected disabled></option>
-                    <option value="%">Wszystkie statusy</option>
-                    <option value="1">Połączony</option>
-                    <option value="0">Niepołączony</option>
+                    <?php
+                    echo "<option value='%' " . ($_GET['status'] == '%' ? 'selected' : ''). ">Wszystkie statusy</option>
+                    <option value='1' " . ($_GET['status'] == '1' ? 'selected' : ''). ">Połączony</option>
+                    <option value='0' " . ($_GET['status'] == '0' ? 'selected' : ''). ">Niepołączony</option>";
+                    ?>
                 </select>
                 <span class="select-highlight"></span>
                 <span class="select-bar"></span>
